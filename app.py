4cd95ba43c3dde4fe51e38f4d262b39f5d5de744
zangetsou <para.plokamar@gmail.com>
2017-12-18 17:16:42 +0200

    I added a new function to check if the user already exists during a registration(email or username). Also if the user signs in correctly, the client receives a json with the user personal info.

--- app.py
+++ app.py
@@ -2,21 +2,34 @@
 from flask import Flask, request
 import authentication
 import sys
+import json
+
+authentication.createTables()
 
 app = Flask(__name__)
 
 @app.route("/app/user", methods =["POST","GET"])
 def index():
-    authentication.createTables()
+    ###SIGN UP
     if request.method == "POST":
         args = request.get_json()
-        authentication.addUser(args)
-        return "Got Post"
+        userExists = authentication.isUserRegistered(args)
+        if userExists:
+            return userExists
+        if authentication.addUser(args):
+            return 'User successfuly registered'
+        else:
+            return 'There was a problem'
+
+    ###LOGIN
     if request.method == "GET":
         username = request.args.get('username')
         password = request.args.get('password')
-        authentication.signInUser(username,password)
-        return "Got Get"
+        user = authentication.signInUser(username,password)
+        if user:
+            return json.dumps(user)
+        else:
+            return "Wrong credentials"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- authentication.py
+++ authentication.py
@@ -1,6 +1,8 @@
 import MySQLdb
 import sys
-import hashlib, uuid
+import hashlib
+import uuid
+
 
 def createTables():
 	db, cursor = connect()
@@ -9,34 +11,29 @@ def createTables():
 	lastname VARCHAR(20), 
 	password VARCHAR(380), 
 	primary_provider VARCHAR(20), 
-	email VARCHAR(80),
-	username VARCHAR(40),
+	email VARCHAR(80) UNIQUE,
+	username VARCHAR(40) UNIQUE,
+	salt VARCHAR(380),
 	photo_url VARCHAR(2083));"""
 
 	cursor.execute(createQuery)
 	db.commit()
 
 
-
 def connect():
-	db = MySQLdb.connect(host = 'localhost',
-					user = 'root',
-					passwd = 'Zangetsou1992',
-					db= 'backend_thesis')
+	db = MySQLdb.connect(host='localhost',
+                      user='root',
+                      passwd='Zangetsou1992',
+                      db='backend_thesis')
 	cur = db.cursor()
-	return db,cur
+	return db, cur
 
 
 def addUser(data):
 
 	db, cur = connect()
 	if data.get('primary_provider') == 'form':
-		query = addFormUser(data)
-
-		ret = cur.execute(query)
-		db.commit()
-		data = cur.fetchone()
-		db.close()
+		return addFormUser(data)
 
 
 def addFormUser(data):
@@ -46,25 +43,62 @@ def addFormUser(data):
 	primary_provider = data.get('primary_provider')
 	email = data.get('email')
 	username = data.get('username')
-	photo_url = data.get('photo_url')
-
+	#producing the unique salt for the user
 	salt = uuid.uuid4().hex
+	#producing the hashed password
 	hashed_password = hashlib.sha512(password + salt).hexdigest()
 
-	q = "INSERT INTO profiles(firstname , lastname, password, primary_provider,email,username,photo_url,salt) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (firstname, lastname, hashed_password, primary_provider, email, username, photo_url, salt)
-	return q
+	q = "INSERT INTO profiles(firstname , lastname, password, primary_provider,email,username,salt) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (
+		firstname, lastname, hashed_password, primary_provider, email, username, salt)
+
+	db, cursor = connect()
+	cursor.execute(q)
+	db.commit()
+	data = cursor.fetchone()
+	db.close()
+
+	if not data:
+		return True
+	else:
+		return data
 
 
 def signInUser(username, password):
-	db,cursor = connect()
-	query = '''SELECT salt FROM profiles WHERE username='%s';'''%(username)
-	ret = cursor.execute(query)
+	db, cursor = connect()
+	query = '''SELECT salt FROM profiles WHERE username='%s';''' % (username)
+	cursor.execute(query)
 	salt = cursor.fetchone()
-	salt = ''.join(salt)
-	hashedPass = hashlib.sha512(password + salt).hexdigest()
-	query2 = '''SELECT firstname,lastname,email FROM profiles WHERE username='%s' AND password='%s';'''%(username,hashedPass)
-	cursor.execute(query2)
-	result = cursor.fetchone()
-	db.commit()
-	print(result, sys.stderr)
-	print(salt)
+	if salt:
+		salt = ''.join(salt)
+		hashedPass = hashlib.sha512(password + salt).hexdigest()
+		query2 = '''SELECT firstname,lastname,email FROM profiles WHERE username='%s' AND password='%s';''' % (
+			username, hashedPass)
+		cursor.execute(query2)
+		result = cursor.fetchone()
+		db.commit()
+		user = {'firstname': result[0], 'lastname': result[1], 'email': result[2]}
+		return user
+	else:
+		return False
+
+
+def isUserRegistered(data):
+	username = data.get('username')
+	email = data.get('email')
+
+	db, cursor = connect()
+	#Check if username exists
+	queryUsername = """SELECT * FROM profiles WHERE username='%s';""" % (username)
+	cursor.execute(queryUsername)
+	result1 = cursor.fetchone()
+	if result1:
+		return 'Username already exists'
+
+	#Check if email exists
+	queryEmail = """SELECT * FROM profiles WHERE email='%s';""" % (email)
+	cursor.execute(queryEmail)
+	result2 = cursor.fetchone()
+	if result2:
+		return 'Email already exists'
+	db.close()
+	return False
