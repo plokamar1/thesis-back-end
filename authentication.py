fbe74d99fb2c79a6a68d1f04f9a5b5d614f850e6
zangetsou <para.plokamar@gmail.com>
2017-12-19 21:35:11 +0200

    on sign in a json with the basic info of the user gets returned.

--- authentication.py
+++ authentication.py
@@ -3,55 +3,11 @@ import sys
 import hashlib
 import uuid
 from flask_sqlalchemy import SQLAlchemy
-from itsdangerous import (TimedJSONWebSignatureSerializer
-                          as Serializer, BadSignature, SignatureExpired)
 from Classes.User import User
-
-
-
-# def createTables():
-# 	profiles = Table('profiles', metadata, autoload=True)
-# 	if not profiles.exists():
-# 		profiles = Table('profiles', metadata,
-#                   Column('id', Integer, primary_key=True,
-#                          nullable=False, autoincrement=True),
-#                   Column('firstname', VARCHAR(20)),
-#                   Column('lastname', VARCHAR(20)),
-#                   Column('password', VARCHAR(380)),
-#                   Column('primary_provider', VARCHAR(20)),
-#                   Column('email', VARCHAR(80), unique=True),
-#                   Column('username', VARCHAR(40)),
-#                   Column('salt', VARCHAR(380)),
-#                   Column('photo_url', VARCHAR(2083))
-#                   )
-# 		profiles.create()
-# db, cursor = connect()
-# createQuery = """CREATE TABLE IF NOT EXISTS profiles(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-# firstname VARCHAR(20),
-# lastname VARCHAR(20),
-# password VARCHAR(380),
-# primary_provider VARCHAR(20),
-# email VARCHAR(80) UNIQUE,
-# username VARCHAR(40) UNIQUE,
-# salt VARCHAR(380),
-# photo_url VARCHAR(2083));"""
-
-# cursor.execute(createQuery)
-# db.commit()
-
-
-def connect():
-	db = MySQLdb.connect(host='localhost',
-                      user='root',
-                      passwd='Zangetsou1992',
-                      db='backend_thesis')
-	cur = db.cursor()
-	return db, cur
-
 
 def addUser(db, data):
 	if data.get('primary_provider') == 'form':
-		return addFormUser(db ,data)
+		return addFormUser(db, data)
 
 
 def addFormUser(db, data):
@@ -82,27 +38,24 @@ def addFormUser(db, data):
 
 def signInUser(_username, _password):
 	#Getting the user object IF there is any with that username
-	user = User.query.filter_by(username = _username).first()
+	user = User.query.filter_by(username=_username).first()
 	if user:
-		email = user.verify_pass(_password)
-		user = {'email': email}
-		return user
+		json = user.verify_pass(_password)
+		return json
 	else:
 		return False
 
 
-def isUserRegistered(db,data):
+def isUserRegistered(db, data):
 	_username = data.get('username')
 	_email = data.get('email')
 	#check username availability
 	results = User.query.filter_by(username=_username).first()
-	print(results , sys.stderr)
-
 	if results:
 		return 'Username already exists'
 
 	#check email availability
-	results = User.query.filter_by(email = _email).first()
+	results = User.query.filter_by(email=_email).first()
 	if results:
 		return 'Email already exists'
 	return False
