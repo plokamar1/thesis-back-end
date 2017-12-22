c41b7f3663f8628b8f5bca140fc7348723580f69
zangetsou <para.plokamar@gmail.com>
2017-12-22 15:24:49 +0200

    Changed the responses to json and status code. also added cors because i was getting Cross Origin request block.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from Classes.User import db
 import authentication
 import sys
 import json
+from flask_cors import CORS
 
 # authentication.createTables()
 
@@ -14,23 +15,29 @@ app.config['SECRET_KEY'] = 'BIMOJI OTO FLAT KNER Punk IPA'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:Zangetsou1992@localhost/backend_thesis'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 db.init_app(app)
 with app.app_context():
 
     db.create_all()
     
-@app.route("/app/user", methods=["POST", "GET"])
+@app.route("/api/user", methods=["POST", "GET"])
 def index():
     ###SIGN UP
     if request.method == "POST":
         args = request.get_json()
         userExists = authentication.isUserRegistered(db ,args)
         if userExists:
-            return userExists
-        if authentication.addUser(db,args):
-            return 'User successfuly registered'
+            print(json.dumps({'error': 'User already exists'}), sys.stderr)
+            return json.dumps({'error': 'User already exists'}), 403
         else:
-            return 'There was a problem'
+            print('new user', sys.stderr)
+            resp = authentication.addUser(db, args)
+            if 'error' in resp:
+                return json.dumps(resp), 403
+            else:
+                return json.dumps(resp), 200
 
     ###LOGIN
     if request.method == "GET":
@@ -42,7 +49,7 @@ def index():
         else:
             abort(400)
 
-@app.route("/fbdata", methods=["POST", "GET"])
+@app.route("/api/fbdata", methods=["POST", "GET"])
 def get_user_data():
     if request.method== "GET":
         userToken = request.args.get('token')
