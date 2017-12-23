4588aac64382a5d94f2a01d16f36c3754764b0bc
zangetsou <para.plokamar@gmail.com>
2017-12-23 22:42:48 +0200

    Started the google authentication through back end only

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 #!flask/bin/python
-from flask import Flask, request, abort
+from flask import Flask, request, abort, g
 from flask_sqlalchemy import SQLAlchemy
-import authFB
-from Classes.User import db
-import authentication
+from requests_oauthlib import OAuth2Session
 import sys
 import json
 from flask_cors import CORS
+
+import authFB
+from Classes.User import db
+import authentication
+
+gglCreds = json.load(open('client_secret.json'))
+scope=['https://www.googleapis.com/auth/drive.metadata.readonly','https://mail.google.com/','profile','https://www.googleapis.com/auth/gmail.readonly ']
+google = OAuth2Session(gglCreds['web']['client_id'], scope = scope, redirect_uri = 'https://localhost:4200/auth/sign-in')
+
 
 # authentication.createTables()
 
@@ -16,6 +23,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:Zangetsou1992@loca
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
 
 db.init_app(app)
 with app.app_context():
@@ -53,19 +62,42 @@ def index():
         user = authentication.signInUser(username, password)
         if user:
             #return user data
-            print(user, sys.stderr)
+            print(json.dumps(user), sys.stderr)
             return json.dumps(user), 200
         else:
             return json.dumps({'message': 'Wrong Credentials'}),403
 
 
-@app.route("/api/fbAuth", methods=["POST", "GET"])
-def get_user_data():
+@app.route("/api/socialAuth", methods=["POST", "GET"])
+def socialAuth():
     if request.method== "GET":
-        userToken = request.args.get('token')
-        resp = authFB.getUserInfo(userToken,db)
-        return json.dumps(resp)
-        
+        prov = request.args.get('prov')
+        if prov == 'fb':
+            userToken = request.args.get('token')
+            resp = authFB.getUserInfo(userToken,db)
+            return json.dumps(resp)
+        if prov == 'ggl':
+            redirect_uri, state = google.authorization_url(gglCreds['web']['auth_uri'], access_type="offline", prompt="select_account")
+            return redirect_uri, 200
+    if request.method == "POST":
+        code = request.get_json().get('code')
+
+
+@app.route('/api/googleuser', methods=['GET'])
+def user_create():
+    code = request.args.get('code')
+    google.fetch_token(gglCreds['web']['token_uri'] , client_secret=gglCreds['web']['client_secret'],code=code)
+    data = google.get('https://www.googleapis.com/gmail/v1/users/me/profile').json()
+    # flow.fetch_token(code = code)
+
+    # session = flow.authorized_session()
+    # g._gsession = session
+    # data = session.get('https://www.googleapis.com/userinfo/v2/me').json()
+    # print(data, sys.stderr)
+
+    # data = session.get('https://www.googleapis.com/gmail/v1/users/me/profile').json()
+
+    return json.dumps(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
